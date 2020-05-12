2ef1880a280ae5c7dbf5263faf0d82382b648fd9
Chheany Mok <cmok4290@gmail.com>
2020-05-12 19:42:28 -0400

    Add background image attribution

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,7 +3,10 @@ import "./home.css";
 
 export default function Home() {
   return (
-    <div>
+    <div className="home">
+      <div className="pexel">
+        <p>Photo by <a href="https://www.pexels.com/@felixmittermeier?utm_content=attributionCopyText&utm_medium=referral&utm_source=pexels">Felix Mittermeier</a> from <a href="https://www.pexels.com/photo/4k-wallpaper-android-wallpaper-astro-astrology-1146134/?utm_content=attributionCopyText&utm_medium=referral&utm_source=pexels">Pexels</a></p>
+      </div>
     </div>
   );
 }
