bc3ccbc4251ca8095d2016f010243971a947fd76
Chheany Mok <cmok4290@gmail.com>
2020-05-13 20:26:34 -0400

    Add tooltips to brand icon

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -9,30 +9,76 @@ import {
   faTwitter
 } from "@fortawesome/free-brands-svg-icons";
 import Nav from "react-bootstrap/Nav";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Tooltip from "react-bootstrap/Tooltip";
 import "./contact.css";
+
+const data = [
+  {
+    "url": "#",
+    "icon": faDiscord,
+    "target": false,
+    "tooltip": "headnodic#0023"
+  },
+  {
+    "url": "https://github.com/cmok4290",
+    "icon": faGithub,
+    "target": true,
+    "tooltip": "github.com/cmok4290"
+  },
+  {
+    "url": "mailto:cmok4290@gmail.com",
+    "icon": faGoogle,
+    "target": true,
+    "tooltip": "cmok4290@gmail.com"
+  },
+  {
+    "url": "https://www.hackerrank.com/profile/headnodic",
+    "icon": faHackerrank,
+    "target": true,
+    "tooltip": "profile/headnodic"
+  },
+  {
+    "url": "https://www.linkedin.com/in/cmok4290",
+    "icon": faLinkedinIn,
+    "target": true,
+    "tooltip": "in/cmok4290"
+  },
+  {
+    "url": "https://twitter.com/cmok4290",
+    "icon": faTwitter,
+    "target": true,
+    "tooltip": "twitter.com/cmok4290"
+  }
+];
+
+function renderTooltip(tooltip, props) {
+  console.log(props);
+  return (
+    <Tooltip id="button-tooltip" {...props}>
+      {tooltip}
+    </Tooltip>
+  );
+}
 
 class Contact extends React.Component {
   render() {
     return (
       <div className="contact">
-        <Nav.Link href="#">
-          <FontAwesomeIcon icon={faDiscord} size="6x" color="white" className="custom-icon" />
-        </Nav.Link>
-        <Nav.Link href="https://github.com/cmok4290" target="_blank">
-          <FontAwesomeIcon icon={faGithub} size="6x" color="white" className="custom-icon" />
-        </Nav.Link>
-        <Nav.Link href="mailto:cmok4290@gmail.com">
-          <FontAwesomeIcon icon={faGoogle} size="6x" color="white" className="custom-icon" />
-        </Nav.Link>
-        <Nav.Link href="https://www.hackerrank.com/profile/headnodic" target="_blank">
-          <FontAwesomeIcon icon={faHackerrank} size="6x" color="white" className="custom-icon" />
-        </Nav.Link>
-        <Nav.Link href="https://www.linkedin.com/in/cmok4290" target="_blank">
-          <FontAwesomeIcon icon={faLinkedinIn} size="6x" color="white" className="custom-icon" />
-        </Nav.Link>
-        <Nav.Link href="https://twitter.com/cmok4290" target="_blank">
-          <FontAwesomeIcon icon={faTwitter} size="6x" color="white" className="custom-icon" />
-        </Nav.Link>
+        {data.map((contact, i) => {
+          return (
+            <OverlayTrigger
+              key={i}
+              placement="top"
+              delay={{ show: 250, hide: 250 }}
+              overlay={renderTooltip(contact.tooltip)}
+            >
+              <Nav.Link href={contact.url} target={contact.target ? "_blank" : ""}>
+                <FontAwesomeIcon icon={contact.icon} size="6x" color="white" className="custom-icon" />
+              </Nav.Link>
+            </OverlayTrigger>
+          );
+        })}
       </div>
     );
   }
