dd5062f845af342743ede325143692d2c38b4bd2
Chheany Mok <cmok4290@gmail.com>
2020-06-23 14:24:20 -0400

    Update contact component styles

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -7,7 +7,7 @@ import {
   faGoogle,
   faHackerrank,
   faLinkedinIn,
-  faTwitter
+  faTwitter,
 } from "@fortawesome/free-brands-svg-icons";
 import Nav from "react-bootstrap/Nav";
 import OverlayTrigger from "react-bootstrap/OverlayTrigger";
@@ -17,47 +17,47 @@ import "./contact.css";
 
 const data = [
   {
-    "url": resume,
-    "icon": faFilePdf,
-    "target": true,
-    "tooltip": "resume"
+    url: resume,
+    icon: faFilePdf,
+    target: true,
+    tooltip: "resume",
   },
   {
-    "url": "#",
-    "icon": faDiscord,
-    "target": false,
-    "tooltip": "headnodic#0023"
+    url: "#",
+    icon: faDiscord,
+    target: false,
+    tooltip: "headnodic#0023",
   },
   {
-    "url": "https://github.com/cmok4290",
-    "icon": faGithub,
-    "target": true,
-    "tooltip": "github.com/cmok4290"
+    url: "https://github.com/cmok4290",
+    icon: faGithub,
+    target: true,
+    tooltip: "github.com/cmok4290",
   },
   {
-    "url": "mailto:cmok4290@gmail.com",
-    "icon": faGoogle,
-    "target": true,
-    "tooltip": "cmok4290@gmail.com"
+    url: "mailto:cmok4290@gmail.com",
+    icon: faGoogle,
+    target: true,
+    tooltip: "cmok4290@gmail.com",
   },
   {
-    "url": "https://www.hackerrank.com/profile/headnodic",
-    "icon": faHackerrank,
-    "target": true,
-    "tooltip": "profile/headnodic"
+    url: "https://www.hackerrank.com/profile/headnodic",
+    icon: faHackerrank,
+    target: true,
+    tooltip: "profile/headnodic",
   },
   {
-    "url": "https://www.linkedin.com/in/cmok4290",
-    "icon": faLinkedinIn,
-    "target": true,
-    "tooltip": "in/cmok4290"
+    url: "https://www.linkedin.com/in/cmok4290",
+    icon: faLinkedinIn,
+    target: true,
+    tooltip: "in/cmok4290",
   },
   {
-    "url": "https://twitter.com/cmok4290",
-    "icon": faTwitter,
-    "target": true,
-    "tooltip": "twitter.com/cmok4290"
-  }
+    url: "https://twitter.com/cmok4290",
+    icon: faTwitter,
+    target: true,
+    tooltip: "twitter.com/cmok4290",
+  },
 ];
 
 function renderTooltip(tooltip, props) {
@@ -73,20 +73,30 @@ class Contact extends React.Component {
   render() {
     return (
       <div className="contact">
-        {data.map((contact, i) => {
-          return (
-            <OverlayTrigger
-              key={i}
-              placement="top"
-              delay={{ show: 250, hide: 250 }}
-              overlay={renderTooltip(contact.tooltip)}
-            >
-              <Nav.Link href={contact.url} target={contact.target ? "_blank" : ""}>
-                <FontAwesomeIcon icon={contact.icon} size="6x" color="white" className="custom-icon" />
-              </Nav.Link>
-            </OverlayTrigger>
-          );
-        })}
+        <div className="content">
+          {data.map((contact, i) => {
+            return (
+              <OverlayTrigger
+                key={i}
+                placement="top"
+                delay={{ show: 250, hide: 250 }}
+                overlay={renderTooltip(contact.tooltip)}
+              >
+                <Nav.Link
+                  href={contact.url}
+                  target={contact.target ? "_blank" : ""}
+                >
+                  <FontAwesomeIcon
+                    icon={contact.icon}
+                    size="6x"
+                    color="white"
+                    className="custom-icon"
+                  />
+                </Nav.Link>
+              </OverlayTrigger>
+            );
+          })}
+        </div>
       </div>
     );
   }
