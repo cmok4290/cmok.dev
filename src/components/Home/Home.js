4d7ff55d126f14ff0141263ffd6b08a950fda7f1
Chheany Mok <cmok4290@gmail.com>
2020-06-23 16:57:59 -0400

    Add particles to home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,22 +2,19 @@ import React from "react";
 import "./home.css";
 
 export default function Home() {
-  return (
-    <div className="home">
-      <div className="footer">
-        <p className="pexel">
-          Photo by{" "}
-          <a href="https://www.pexels.com/@felixmittermeier?utm_content=attributionCopyText&utm_medium=referral&utm_source=pexels">
-            Felix Mittermeier
-          </a>{" "}
-          from{" "}
-          <a href="https://www.pexels.com/photo/4k-wallpaper-android-wallpaper-astro-astrology-1146134/?utm_content=attributionCopyText&utm_medium=referral&utm_source=pexels">
-            Pexels
-          </a>
-        </p>
-      </div>
-    </div>
-  );
+  const div = document.createElement("div");
+  div.id = "particles-js";
+  document.body.appendChild(div);
+
+  const particles = document.createElement("script");
+  particles.src = "js/particles.js";
+  document.body.appendChild(particles);
+
+  const loader = document.createElement("script");
+  loader.src = "js/app.js";
+  document.body.appendChild(loader);
+
+  return <div className="home"></div>;
 }
 
 /**
