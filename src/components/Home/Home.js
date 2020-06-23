018f7513d03dc9594bc154571ccbe5e618fa9b27
Chheany Mok <cmok4290@gmail.com>
2020-06-23 13:24:06 -0400

    Update home component styles

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,9 +4,23 @@ import "./home.css";
 export default function Home() {
   return (
     <div className="home">
-      <div className="pexel">
-        <p>Photo by <a href="https://www.pexels.com/@felixmittermeier?utm_content=attributionCopyText&utm_medium=referral&utm_source=pexels">Felix Mittermeier</a> from <a href="https://www.pexels.com/photo/4k-wallpaper-android-wallpaper-astro-astrology-1146134/?utm_content=attributionCopyText&utm_medium=referral&utm_source=pexels">Pexels</a></p>
+      <div className="footer">
+        <p className="pexel">
+          Photo by{" "}
+          <a href="https://www.pexels.com/@felixmittermeier?utm_content=attributionCopyText&utm_medium=referral&utm_source=pexels">
+            Felix Mittermeier
+          </a>{" "}
+          from{" "}
+          <a href="https://www.pexels.com/photo/4k-wallpaper-android-wallpaper-astro-astrology-1146134/?utm_content=attributionCopyText&utm_medium=referral&utm_source=pexels">
+            Pexels
+          </a>
+        </p>
       </div>
     </div>
   );
 }
+
+/**
+        <p>Photo by <a href="https://www.pexels.com/@felixmittermeier?utm_content=attributionCopyText&utm_medium=referral&utm_source=pexels">Felix Mittermeier</a> from <a href="https://www.pexels.com/photo/4k-wallpaper-android-wallpaper-astro-astrology-1146134/?utm_content=attributionCopyText&utm_medium=referral&utm_source=pexels">Pexels</a></p>
+ *
+ */
